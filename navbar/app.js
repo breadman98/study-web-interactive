14e375263baa395ef6205b96726f101dd6ef18eb
chheze <swo98@naver.com>
2021-10-11 20:09:56 +0900

    Delete app.js

--- navbar/app.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".navbar_burger");
-const menu = document.querySelector(".navbar_menu");
-const icon = document.querySelector(".navbar_icon");
-
-
-button.addEventListener("click",()=>{
-     menu.classList.toggle("active");
-     icon.classList.toggle("active");
-});
