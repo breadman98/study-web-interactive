71539ab0def178590f2a34e3a9493f990405b61b
chheze <swo98@naver.com>
2021-10-12 18:15:12 +0900

    Delete navbar directory

--- navbar/app.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector(".navbar-burger");
-const menu = document.querySelector(".navbar-menu");
-const icon = document.querySelector(".navbar-icon");
-
-burger.addEventListener("click",function(){
-     menu.classList.toggle("active");
-     icon.classList.toggle("active");
-    
-})
