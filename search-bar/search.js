5831ffb45d9b710c440e177144df53466e37c96a
chheze <swo98@naver.com>
2021-11-02 21:13:19 +0900

    Add files via upload

---
+++ search-bar/search.js
@@ -0,0 +1,16 @@
+const searchBox = document.querySelector(".search-box");
+const icon = document.querySelector(".icon");
+
+icon.addEventListener("click",function(){
+     searchBox.classList.toggle("active");
+     icon.classList.toggle("active");
+})
+
+
+const searchForm = document.querySelector(".search-box");
+const searchInput = document.querySelector(".search-box input");
+
+searchForm.addEventListener("submit",function(e){
+     e.preventDefault();
+     location.href="https://www.google.co.kr/search?q="+searchInput.value;
+})
